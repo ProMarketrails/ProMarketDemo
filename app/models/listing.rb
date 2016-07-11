9563d62b757b06b7db75c2c8bcbacdfee22b02c7
harish <harish@allwyncorp.com>
2016-07-11 11:22:50 -0400

    Add Validations for Users and Listngs

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -1,4 +1,16 @@
 class Listing < ActiveRecord::Base
 	 has_attached_file :image, styles: { medium: "200x", thumb: "100x100>" }, default_url: "default.jpg"
 	 validates_attachment :image, content_type: { content_type: /\Aimage\/.*\Z/ }
-end
+	 validates :name, :description, :price, presence: true	 
+	 validates :price, numericality: {greater_than: 0}
+	 validates_attachment_presence :image
+ end
+
+
+
+
+
+
+
+
+
